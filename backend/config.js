ab6fd5e604381893db40368b4dfaaf440a03be29
20062573 <20062573@mydbs.ie>
2025-12-07 11:53:44 +0000

    Create config.js

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    PORT: 5001,
+    JWT_SECRET: "bhargavxyz",
+    MONGO_URI: "mongodb+srv://bhargavmodugula2003:bhargavmodugula2003@ridehub.upnutls.mongodb.net/ridehub?retryWrites=true&w=majority",
+  };
+  
