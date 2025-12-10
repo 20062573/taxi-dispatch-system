29f0aacd8092b26a699145ecb14e5d1aabd1be01
20062573 <20062573@mydbs.ie>
2025-12-10 19:25:09 +0000

    Create axios.js
    for api

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+// custom axios client for all api calls (ref: w3schools.com)
+const instance = axios.create({
+  baseURL: "http://localhost:5001/api",
+});
+
+// adds token to every request if logged in
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token"); // get saved jwt
+  if (token) config.headers.Authorization = `Bearer ${token}`; // attach token (ref: ai)
+  return config;
+});
+
+export default instance; // use this everywhere (ref: w3)
