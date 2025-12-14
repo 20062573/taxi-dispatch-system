e696021eb93b7d2bf6df5bdccc4d7fccde26d155
20062573 <20062573@mydbs.ie>
2025-12-14 09:34:20 +0000

    Update config.js
    for the use of env

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    PORT: 5001,
-    JWT_SECRET: "bhargavxyz",
-    MONGO_URI: "mongodb+srv://bhargavmodugula2003:bhargavmodugula2003@ridehub.upnutls.mongodb.net/ridehub?retryWrites=true&w=majority",
-  };
+  PORT: process.env.PORT || 5001,   // ref: https://www.w3schools.com/nodejs/nodejs_env_variables.asp
+  JWT_SECRET: process.env.JWT_SECRET,
+  // This should always be stored in .env (never hard-coded) ref: https://stackoverflow.com/questions/31309759/what-is-the-purpose-of-jwt-secret
+  MONGO_URI: process.env.MONGO_URI, // Loaded from environment variables for security reasons ref: https://stackoverflow.com/questions/42415103/how-to-hide-mongodb-credentials-in-node-js
+};
   
